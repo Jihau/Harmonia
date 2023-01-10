edd903b10e27dd33b57152556e9a39aebaec2d6a
Johanna Toivanen <johanna.toivanen@metropolia.fi>
2023-01-10 13:43:19 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.harmonia {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.harmonia to javafx.fxml;
+    exports com.example.harmonia;
+}
