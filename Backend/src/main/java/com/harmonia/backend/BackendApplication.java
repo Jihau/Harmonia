563b8258af46a9a17cfaaf0d01c2177c465d6b6a
Johanna Toivanen <johanna.toivanen@metropolia.fi>
2023-02-12 22:59:06 +0200

    User controller service repository

--- Backend/src/main/java/com/harmonia/backend/BackendApplication.java
+++ Backend/src/main/java/com/harmonia/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.harmonia.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
