14dc4bf043ecc7629b501bde3a6d72c801b4c819
Mohammed Al-Jewari <mohammja@metropolia.fi>
2023-02-17 19:07:17 +0200

    User login

--- Backend/src/main/java/com/harmonia/backend/repository/UserRepository.java
+++ Backend/src/main/java/com/harmonia/backend/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.harmonia.backend.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
 
 }
