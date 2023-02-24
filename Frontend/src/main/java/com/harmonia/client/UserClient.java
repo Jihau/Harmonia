db44d976cdafb3ea740eae28bdb43a4776c076b8
Sampo <sampoo@metropolia.fi>
2023-02-24 18:52:56 +0200

    intermediate commit for merge

--- Frontend/src/main/java/com/harmonia/client/UserClient.java
+++ Frontend/src/main/java/com/harmonia/client/UserClient.java
@@ -1,14 +1,23 @@
 package com.harmonia.client;
 
 import com.harmonia.po.UserPO;
+import com.harmonia.po.ValidationPOJO;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.spi.IIOServiceProvider;
 
 public class UserClient {
     private static final String BASE_URL = "http://localhost:8080/user";
@@ -38,7 +47,55 @@ public class UserClient {
         ResponseEntity<UserPO> response = request(user, HttpMethod.DELETE);
 
         return response;
-    } 
+    }
+
+    public ResponseEntity<UserPO> putUser(UserPO user) {
+
+        ResponseEntity<UserPO> response = request(user,HttpMethod.PUT);
+
+        return response;
+    }
+
+    public ResponseEntity<UserPO> editPassword(UserPO user) {
+
+        //This lacks validation!!!
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("Content-Type", "application/json");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        HttpEntity<UserPO> request = new HttpEntity<>(user, headers);
+        
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(user.getUserId()));
+
+        ResponseEntity<UserPO> response = restTemplate.exchange(BASE_URL+"/"+user.getUserId()+"/password", HttpMethod.PUT, request,
+                UserPO.class, params);
+        return response;
+    }
+
+    public ResponseEntity<UserPO> editProfileIcon(UserPO user) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("Content-Type", "application/json");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        HttpEntity<UserPO> request = new HttpEntity<>(user, headers);
+        
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(user.getUserId()));
+
+        ResponseEntity<UserPO> response = restTemplate.exchange(BASE_URL+"/"+user.getUserId()+"/password", HttpMethod.PUT, request,
+                UserPO.class, params);
+        return response;
+    }
 
     private static ResponseEntity<UserPO> request(UserPO user, HttpMethod method ) {
         HttpHeaders headers = new HttpHeaders();
@@ -56,5 +113,27 @@ public class UserClient {
         ResponseEntity<UserPO> response = restTemplate.exchange(BASE_URL, method, request,
                 UserPO.class, params);
         return response;
+    }
+
+    public static HttpStatusCode validate(String username, String passwrd) {
+    
+    System.out.println("Validating for \nusername: " + username + "\npassword: " + passwrd);
+
+        ValidationPOJO validateMe = new ValidationPOJO(username, passwrd);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        HttpEntity<ValidationPOJO> request = new HttpEntity<>(validateMe ,headers);
+
+        ResponseEntity<UserPO> response = restTemplate.exchange(BASE_URL+"/login", HttpMethod.POST, request, UserPO.class);
+
+
+        return response.getStatusCode();
+
     }
 }
---
+++ Frontend/src/main/java/com/harmonia/controller/SettingsController.java
@@ -0,0 +1,140 @@
+package com.harmonia.controller;
+
+import java.io.IOException;
+
+import com.harmonia.HarmoniaApplication;
+import com.harmonia.client.UserClient;
+import com.harmonia.po.*;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class SettingsController {
+    private UserPO user;
+
+    @FXML
+    AnchorPane root;
+
+    @FXML
+    TextField UsernameField;
+
+    @FXML
+    TextField profImgField;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    PasswordField oldPasswordField;
+
+    @FXML
+    Button submitButton;
+
+    public void initialize() {
+
+        /* placeholder, get user from session */
+
+        System.out.println("initializing");
+
+        this.user = new UserPO();
+
+        this.user.setUserId(1);
+        this.user.setUsername("marko1");
+        this.user.setEmail("Marko@gmail.com");
+        this.user.setProfileIcon("https://vignette.wikia.nocookie.net/awesomeanimeandmanga/images/3/34/K-on%21-avatar-200x200.jpg/revision/latest?cb=20110517050049");
+        this.user.setPassword("marko123");
+
+        
+        /* placeholder, get user from session */
+
+        UsernameField.setText(this.user.getUsername());
+        profImgField.setText(this.user.getProfileIcon());
+
+    }
+
+    public void onSaveButtonClick() {
+
+        /* I don't think this will work after getting the userdata from the database because of password hashing. We need to either hash the current password or validate this request with the backend */
+        UserPO user = this.getUser();
+
+        System.out.println(user);
+
+        System.out.println("Updating user:");
+
+        if (!passwordField.getText().equals(null) && oldPasswordField.getText().equals(user.getPassword())) {
+
+            System.out.println("new password detected and old password checked");
+
+            try {
+
+                System.out.println("Trying to put with new password");
+
+                UserClient userclient = new UserClient();
+
+                System.out.println("Client opened");
+
+                System.out.println(UserClient.validate(user.getUsername(), user.getPassword()).value());
+
+                if (UserClient.validate(user.getUsername(), user.getPassword()).value()==200) 
+                user.setUsername(UsernameField.getText());
+                user.setProfileIcon(profImgField.getText());
+                user.setPassword(passwordField.getText());
+
+                userclient.editPassword(user);
+
+                Alert UpdatedAlert = new Alert(AlertType.CONFIRMATION);
+                UpdatedAlert.setHeaderText("User updated");
+                UpdatedAlert.setContentText("Your user details were succesfully updated.");
+                UpdatedAlert.setTitle("Success");
+                UpdatedAlert.showAndWait();
+
+                
+            } catch (Exception e) {
+                System.out.println("Failed to update user");
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Trying to put without new password");
+            try {
+                UserClient userclient = new UserClient();
+                
+                user.setUsername(UsernameField.getText());
+                user.setProfileIcon(profImgField.getText());
+
+                userclient.putUser(user);
+
+                Alert UpdatedAlert = new Alert(AlertType.CONFIRMATION);
+                UpdatedAlert.setHeaderText("User updated");
+                UpdatedAlert.setContentText("Your user details were succesfully updated.");
+                UpdatedAlert.setTitle("Success");
+                UpdatedAlert.showAndWait();
+                
+            } catch (Exception e) {
+                System.out.println("Failed to update user");
+            }
+        }
+    }
+
+    private UserPO getUser() {
+        return this.user;
+    }
+
+    @FXML
+    public void onReturnButtonClick() throws IOException {
+            
+            FXMLLoader loader = new FXMLLoader(HarmoniaApplication.class.getResource("harmonia-view.fxml"));
+            Stage stage = (Stage) root.getScene().getWindow();
+            Scene scene = new Scene(loader.load(), 1280, 720);
+            stage.setScene(scene);
+            stage.setTitle("Harmonia");
+            stage.show();
+    }
+}
