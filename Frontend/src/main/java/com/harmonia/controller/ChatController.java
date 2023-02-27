a74fc98c69e602887b518919229d050263730ada
Jesper Ojala <96197093+Jesperilmari@users.noreply.github.com>
2023-02-27 22:37:17 +0200

    Message handling... For SAMPO

--- Frontend/src/main/java/com/harmonia/controller/ChatController.java
+++ Frontend/src/main/java/com/harmonia/controller/ChatController.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 import java.util.*;
 
 import com.harmonia.HarmoniaApplication;
-import com.harmonia.model.Message;
+import com.harmonia.client.DirectMessageClient;
+import com.harmonia.po.MessagePO;
 import com.harmonia.view.ChatView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ChatController {
     private ChatView view;
-    private List<Message> messages;
+    private List<MessagePO> messages;
 
     /**
      navigation button for nav menu
@@ -68,16 +74,31 @@ public class ChatController {
     @FXML
     private Button fmCommunityBtn;
 
-    public ChatController(ChatView view) {
-        this.view = view;
+    @FXML
+    private TextField sendMessageField;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    private ListView ChatListView;
+
+    private DirectMessageClient messageClient;
+
+    ObservableList<MessagePO> conversationObject = FXCollections.observableArrayList();
+    ObservableList<String> conversationString = FXCollections.observableArrayList();
+
+
+    public void initialize() {
         this.messages = new ArrayList<>();
+        this.messageClient = new DirectMessageClient();
     }
 
     public ChatController(){
 
     }
 
-    public void sendMessage(Message message) {
+    public void sendMessage(MessagePO message) {
         // Add the message to the list and update the view
     }
 
@@ -109,5 +130,30 @@ public class ChatController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    protected void populateListView(){
+        conversationObject.clear();
+        for(MessagePO m : messageClient.getAllMessages().getBody()){
+            System.out.println(m);
+            conversationObject.add(m);
+        }
+        convertList();
+        ChatListView.setItems(conversationString);
+    }
+
+    @FXML
+    public void onSendBtnClick(){
+        populateListView();
+    }
+
+    protected void convertList(){
+        conversationString.clear();
+        for (MessagePO m : conversationObject){
+            conversationString.add(m.toString());
+        }
+        //System.out.println(conversationObject);
+    }
+    protected void drawListView(){
+        ChatListView.setItems(conversationString);
     }
 }
