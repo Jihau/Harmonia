bcd3829f513b686d848d21722ae631de2bda0785
Sampo <sampoo@metropolia.fi>
2023-02-27 17:31:52 +0200

    error message on failed login

--- Frontend/src/main/java/com/harmonia/controller/LoginController.java
+++ Frontend/src/main/java/com/harmonia/controller/LoginController.java
@@ -31,33 +31,18 @@ public class LoginController {
     @FXML
     Label errorLabel;
 
-    public boolean login(String username, String password) {
-        return UserClient.validate(username, password).value()==200;     
-    }
 
-    @FXML
-    public void logInRedirect() {
-        try {
-            FXMLLoader loader = new FXMLLoader(HarmoniaApplication.class.getResource("harmonia-view.fxml"));
-            Stage stage = (Stage) loginButton.getScene().getWindow();
-            Scene scene = new Scene(loader.load(), 1280, 720);
-            stage.setScene(scene);
-            stage.setTitle("Harmonia");
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }   
-    }
+    
 
     @FXML
     public void onLoginButtonClick() {
     
     if (usernameField.getText()=="" || passwordField.getText()=="") {errorLabel.setText("Please fill both fields!"); errorLabel.setVisible(true); return;}
+        errorLabel.setText("Login failed");
+        errorLabel.setVisible(true);
         if (login(usernameField.getText(), passwordField.getText())) {
             logInRedirect();
         }
-        errorLabel.setText("Login failed");
-        errorLabel.setVisible(true);
     }
 
     @FXML
@@ -74,6 +59,22 @@ public class LoginController {
         }
     }
 
-    
+    @FXML
+    public void logInRedirect() {
+        try {
+            FXMLLoader loader = new FXMLLoader(HarmoniaApplication.class.getResource("harmonia-view.fxml"));
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            Scene scene = new Scene(loader.load(), 1280, 720);
+            stage.setScene(scene);
+            stage.setTitle("Harmonia");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
+    }
+
+    public boolean login(String username, String password) {
+        return UserClient.validate(username, password).value()==200;     
+    }
 
 }
