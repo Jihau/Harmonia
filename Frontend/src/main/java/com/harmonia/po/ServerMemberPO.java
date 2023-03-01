edea4a6e455e68d01393dca0eeb465a4fef09cb0
Johanna Toivanen <johanna.toivanen@metropolia.fi>
2023-03-01 22:13:02 +0200

    Adding PO classes for Server, Channel, ServerMembers. Creating method to list all servers on ServerClient Adding a test to list all servers and mock it

---
+++ Frontend/src/main/java/com/harmonia/po/ServerMemberPO.java
@@ -0,0 +1,115 @@
+package com.harmonia.po;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ServerMemberPO implements Serializable {
+    int serverId;
+    int memberId;
+    Long serverMemberId;
+    Long userId;
+    String username;
+    String email;
+    String profileIcon;
+    String bio;
+    UserPO member;
+    Date timestamp;
+    String nickName;
+    Date joinDate;
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public UserPO getMember() {
+        return member;
+    }
+
+    public void setMember(UserPO member) {
+        this.member = member;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getServerId() {
+        return serverId;
+    }
+
+    public void setServerId(int serverId) {
+        this.serverId = serverId;
+    }
+
+    public Long getServerMemberId() {
+        return serverMemberId;
+    }
+
+    public void setServerMemberId(Long serverMemberId) {
+        this.serverMemberId = serverMemberId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileIcon() {
+        return profileIcon;
+    }
+
+    public void setProfileIcon(String profileIcon) {
+        this.profileIcon = profileIcon;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+}
