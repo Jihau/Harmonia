1a22174349b63a9669b85a36281fe7ce6ee27e28
Johanna Toivanen <johanna.toivanen@metropolia.fi>
2023-03-01 20:33:23 +0200

    Tests for ServerService Mocking test in EditUserClientTest

--- Backend/src/main/java/com/harmonia/backend/service/ServerService.java
+++ Backend/src/main/java/com/harmonia/backend/service/ServerService.java
@@ -6,30 +6,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+/**
+ * Service class for managing servers.
+ */
 @Service
 public class ServerService {
     @Autowired
     private ServerRepository serverRepository;
-
+    /**
+     * Constructor for injecting dependencies.
+     *
+     * @param serverRepository the server repository to use
+     */
     public ServerService(ServerRepository serverRepository){
         this.serverRepository = serverRepository;
     }
-
+    /**
+     * Adds a new server to the system.
+     *
+     * @param server the server to add
+     * @return the added server
+     * @throws IllegalArgumentException if the server name is null or empty
+     */
     public Server addServer(Server server){
         if (server.getServerName() == null || server.getServerName().trim().length() == 0){
             return null;
         }
         return serverRepository.save(server);
     }
-
+    /**
+     * Returns a list of all servers in the system.
+     *
+     * @return a list of servers
+     */
     public Iterable<Server> listServers(){return serverRepository.findAll();}
-
+    /**
+     * Deletes a server from the system.
+     *
+     * @param serverId the ID of the server to delete
+     */
     public void deleteServer(Long serverId) {
         serverRepository.deleteById(serverId);
         System.out.println("Server with ID: " + serverId + " is deleted.");
     }
-
+    /**
+     * Edits the name and category of a server.
+     *
+     * @param serverId the ID of the server to edit
+     * @param serverName the new name for the server
+     * @param serverCategory the new category for the server
+     * @throws IllegalArgumentException if the server with the given ID does not exist
+     */
     public void editServerName(Long serverId, String serverName, String serverCategory) {
         Optional<Server> server = serverRepository.findById(serverId);
         if (server.isPresent()){
@@ -41,15 +68,30 @@ public class ServerService {
             throw new IllegalArgumentException("Can't change the name");
         }
     }
-
+    /**
+     * Returns a list of servers with names matching the given search string.
+     *
+     * @param serverName the search string to match against server names
+     * @return a list of matching servers
+     */
     public Iterable<Server> listServersByServerName(String serverName){
         return serverRepository.listServersByServerName("%" + serverName +"%");
     }
-
+    /**
+     * Returns a list of servers owned by the given user.
+     *
+     * @param userId the ID of the user who owns the servers
+     * @return a list of matching servers
+     */
     public Iterable<Server> listServersByOwnerId(Long userId){
         return serverRepository.listServersByOwnerId(userId);
     }
-
+    /**
+     * Returns a list of servers with categories matching the given search string.
+     *
+     * @param serverCategory the search string to match against server categories
+     * @return a list of matching servers
+     */
     public Iterable<Server> listServersByServerCategory(String serverCategory){
         return serverRepository.listServersByServerCategory("%" + serverCategory +"%");
     }
--- Frontend/src/test/java/com/harmonia/client/EditUserClientTest.java
+++ Frontend/src/test/java/com/harmonia/client/EditUserClientTest.java
@@ -2,19 +2,41 @@ package com.harmonia.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.harmonia.po.UserPO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 
 public class EditUserClientTest {
-  /*  @InjectMocks
+    @InjectMocks
     UserClient userClient;
-      */
-    UserClient userClient = new UserClient();
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    public void editUserTest() throws JsonProcessingException {
+    public void editUserTest()  {
         UserPO request = new UserPO();
         request.setUserId(1);
         request.setProfileIcon("https://i.imgur.com/yfhVP8e.png");
+        when(restTemplate.exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), Mockito.<Class<UserPO>>any(), anyMap()))
+                .thenReturn(new ResponseEntity<>((request), HttpStatus.OK));
         UserPO responseUserPO = userClient.editIcon(request);
         assertEquals(request.getProfileIcon(), responseUserPO.getProfileIcon());
     }
