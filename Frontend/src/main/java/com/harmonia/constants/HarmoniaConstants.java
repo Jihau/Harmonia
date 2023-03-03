6dedb55c14c987a2f387ab98a4ceea568dcd8978
Sampo <sampoo@metropolia.fi>
2023-03-03 17:28:25 +0200

    added constants

--- Frontend/src/main/java/com/harmonia/constants/HarmoniaConstants.java
+++ Frontend/src/main/java/com/harmonia/constants/HarmoniaConstants.java
@@ -20,12 +20,12 @@ public class HarmoniaConstants {
     public static final String CHANNEL_EDIT_URL = BASE_URL + "/channel/{channelId}";
     public static final String SERVER_LIST_BY_NAME_URL = BASE_URL + "/server/name/{serverName}";
     public static final String SERVER_LIST_BY_MEMBER_ID_URL = BASE_URL + "/member/memberId/{memberId}";
-    public static UserPO LOGGED_USERS = null;
     public static final String MEMBERS_BY_SERVER_ID_URL = BASE_URL + "/members/{serverId}";
-
     public static final String PM_GETALL_URL = BASE_URL + "/pmessage";
     public static final String PM_ADD_URL = BASE_URL + "/pmessage";
     public static final String PM_REMOVE_URL = BASE_URL + "/pmessage";
     public static final String PM_EDIT_URL = BASE_URL + "/pmessage/{pMessageId}";
+    
+    public static UserPO LOGGED_USERS = null;
     
 }
