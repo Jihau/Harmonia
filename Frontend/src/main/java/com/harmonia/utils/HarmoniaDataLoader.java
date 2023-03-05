a70a0f8918df73ae0451bc274fe80b080f46d60b
mohammja <mohammja@metropolia.fi>
2023-03-05 23:15:52 +0200

    Documentation utilities classes.

--- Frontend/src/main/java/com/harmonia/utils/HarmoniaDataLoader.java
+++ Frontend/src/main/java/com/harmonia/utils/HarmoniaDataLoader.java
@@ -7,19 +7,38 @@ import com.harmonia.constants.HarmoniaData;
 import com.harmonia.po.MessagePO;
 import com.harmonia.po.UserPO;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Objects;
 
+
+/**
+ * This class contains methods to load user data, load direct messages between users, and interact with direct messages.
+ *
+ * @author Harmonia team
+ * @version 1.0
+ */
 public class HarmoniaDataLoader {
+
+    /**
+     * A client for interacting with the user service.
+     */
     private static final UserClient userClient = new UserClient();
+
+    /**
+     * A client for interacting with the direct message service.
+     */
     private static final DirectMessageClient directMessageClient = new DirectMessageClient();
 
+    /**
+     * Loads users whose username contains the specified text and adds them to the USERS_LIST.
+     *
+     * @param userName the text to search for in usernames
+     */
     public static void searchUserByUsername(String userName) {
         UserPO[] users = userClient.listUsers();
         HarmoniaData.USERS_LIST.clear();
-        for (int i = 0; i < users.length; i++) {
-            HarmoniaData.USERS_LIST.add(users[i]);
-        }
+        Collections.addAll(HarmoniaData.USERS_LIST, users);
     }
 
     /**
@@ -31,8 +50,7 @@ public class HarmoniaDataLoader {
         Comparator<MessagePO> comparator = Comparator.comparingInt(MessagePO::getDmessageId);
         HarmoniaData.DIRECT_MESSAGES_LIST.clear();
         for (MessagePO m : Objects.requireNonNull(directMessageClient.getMessagesByRecipientID(recipientId).getBody())) {
-            if (m.getAuthorId() == recipientId)
-                HarmoniaData.DIRECT_MESSAGES_LIST.add(m);
+            if (m.getAuthorId() == recipientId) HarmoniaData.DIRECT_MESSAGES_LIST.add(m);
         }
         for (MessagePO m : Objects.requireNonNull(directMessageClient.getMessagesByAuthorId(authorId).getBody())) {
             if (m.getRecipientId() == recipientId) {
@@ -47,7 +65,7 @@ public class HarmoniaDataLoader {
      * Sends a message to the recipient specified in the message object and returns the response from the message client.
      *
      * @param messageText the message to be sent
-     * @return the response from the message client
+     * @return true if the message was successfully sent, false otherwise
      */
     public static boolean sendDirectMessage(String messageText) {
         MessagePO newMessage = new MessagePO();
@@ -57,6 +75,12 @@ public class HarmoniaDataLoader {
         return directMessageClient.addMessage(newMessage).getStatusCode().is2xxSuccessful();
     }
 
+    /**
+     * Edits the currently selected direct message with the specified text.
+     *
+     * @param messageText the new text of the message
+     * @return true if the message was successfully edited, false otherwise
+     */
     public static boolean editDirectMessage(String messageText) {
         if (messageText == null || messageText.isBlank()) {
             return false;
@@ -65,6 +89,11 @@ public class HarmoniaDataLoader {
         return directMessageClient.editMessage(HarmoniaData.SELECTED_DIRECT_MESSAGE).getStatusCode().is2xxSuccessful();
     }
 
+    /**
+     * Deletes the currently selected direct message.
+     *
+     * @return true if the message was successfully deleted, false otherwise
+     */
     public static boolean deleteDirectMessage() {
         if (HarmoniaData.SELECTED_DIRECT_MESSAGE == null) {
             return false;
