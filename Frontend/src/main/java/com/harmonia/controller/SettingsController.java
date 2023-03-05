55a6fd0b1e23561cd9f4b695ec31141cd3061cf5
mohammja <mohammja@metropolia.fi>
2023-03-05 20:43:56 +0200

    Settings controller refactoring and documentation.

--- Frontend/src/main/java/com/harmonia/controller/SettingsController.java
+++ Frontend/src/main/java/com/harmonia/controller/SettingsController.java
@@ -1,84 +1,84 @@
 package com.harmonia.controller;
 
-import java.io.IOException;
-import java.util.Objects;
-
-import com.harmonia.HarmoniaApplication;
-import com.harmonia.client.ServerClient;
-import com.harmonia.client.ServerMemberClient;
 import com.harmonia.client.UserClient;
 import com.harmonia.constants.HarmoniaConstants;
-import com.harmonia.constants.HarmoniaConstants.*;
-import com.harmonia.po.*;
-
-import javafx.event.ActionEvent;
-import javafx.event.Event;
+import com.harmonia.po.UserPO;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import org.springframework.http.HttpStatus;
 
+
+/**
+ * Controller class for the Settings view. Allows users to edit their profile image.
+ * Extends MainViewController to enable return to home page functionality.
+ *
+ * @author Harmonia team
+ * @version 1.0
+ */
 public class SettingsController extends MainViewController {
 
-    @FXML
-    private UserPO user;
-
+    /**
+     * The client used to interact with the User API.
+     */
     UserClient userclient = new UserClient();
+
+    /**
+     * The root node of the view.
+     */
     @FXML
     AnchorPane root;
 
+    /**
+     * The text field for the user to input their profile picture URL.
+     */
     @FXML
     TextField profImgField;
 
+    /**
+     * The button to submit the new profile picture URL.
+     */
     @FXML
     Button submitButton;
 
+    /**
+     * The user object representing the currently logged-in user.
+     */
+    @FXML
+    private UserPO user;
+
+    /**
+     * Initializes the view by setting the profile image text field to the user's current profile image.
+     */
     public void initialize() {
 
         /* placeholder, get user from session */
         System.out.println("initializing");
         this.user = HarmoniaConstants.LOGGED_USERS;
-        
+
         /* placeholder, get user from session */
         profImgField.setText(this.user.getProfileIcon());
     }
 
+    /**
+     * Handles the event when the user clicks the "Save" button. Updates the user's profile image in the database
+     * and updates the displayed profile image if the update was successful.
+     */
     public void onSaveButtonClick() {
-            try {
-                UserPO userPO = new UserPO();
-                userPO.setUserId(HarmoniaConstants.LOGGED_USERS.getUserId());
-                userPO.setProfileIcon(profImgField.getText());
-                HarmoniaConstants.LOGGED_USERS = userclient.editIcon(userPO);
-                profImgField.setText(HarmoniaConstants.LOGGED_USERS.getProfileIcon());
-            } catch (Exception e) {
-                e.printStackTrace();
-                Alert failedalert = new Alert(AlertType.ERROR);
-                failedalert.setContentText("Failed to update user");
-                failedalert.show();
-                System.out.println("Failed to update user");
-            }
+        try {
+            UserPO userPO = new UserPO();
+            userPO.setUserId(HarmoniaConstants.LOGGED_USERS.getUserId());
+            userPO.setProfileIcon(profImgField.getText());
+            HarmoniaConstants.LOGGED_USERS = userclient.editIcon(userPO);
+            profImgField.setText(HarmoniaConstants.LOGGED_USERS.getProfileIcon());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Alert failedalert = new Alert(AlertType.ERROR);
+            failedalert.setContentText("Failed to update user");
+            failedalert.show();
+            System.out.println("Failed to update user");
         }
-
-
-    private UserPO getUser() {
-        return this.user;
-    }
-
-    @FXML
-    public void onReturnButtonClick(ActionEvent event) throws IOException {
-            super.onfmHomePageBtnClick(event);
-    }
-
-    private void successAlert() {
-        Alert UpdatedAlert = new Alert(AlertType.CONFIRMATION);
-        UpdatedAlert.setHeaderText("User updated");
-        UpdatedAlert.setContentText("Your user details were succesfully updated.");
-        UpdatedAlert.setTitle("Success");
-        UpdatedAlert.showAndWait();
     }
 }
