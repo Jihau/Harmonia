b17ca32ae50a4057db2e4b3f9be19bb385bb5b15
Jesper Ojala <96197093+Jesperilmari@users.noreply.github.com>
2023-04-02 16:24:30 +0300

    textConstants.java added

---
+++ Frontend/src/main/java/com/harmonia/constants/textConstants.java
@@ -0,0 +1,28 @@
+package com.harmonia.constants;
+
+public class textConstants {
+    public static final String homeText = HarmoniaConstants.texts.getString("homeText");
+    public static final String serversText = HarmoniaConstants.texts.getString("serversText");
+    public static final String dmText = HarmoniaConstants.texts.getString("dmText");
+    public static final String profileText = HarmoniaConstants.texts.getString("profileText");;
+    public static final String settingsText = HarmoniaConstants.texts.getString("settingsText");;
+    public static final String logoutText = HarmoniaConstants.texts.getString("logoutText");;
+    public static final String searchCommunitiesHintText = HarmoniaConstants.texts.getString("searchCommunitiesHintText");;
+    public static final String usernameLabelText = HarmoniaConstants.texts.getString("usernameLabelText");;
+    public static final String passwordLabelText = HarmoniaConstants.texts.getString("passeordLabelText");;
+    public static final String registerLinkText = HarmoniaConstants.texts.getString("registerLinkText");;
+    public static final String loginText = HarmoniaConstants.texts.getString("loginText");;
+    public static final String emailLabelText = HarmoniaConstants.texts.getString("emailLabelText");;
+    public static final String repeatPasswordLabelText = HarmoniaConstants.texts.getString("repeatPasswordLabelText");
+    public static final String alreadyHaveAccountLinkText = HarmoniaConstants.texts.getString("alreadyHaveAccountLinkText");
+    public static final String registerText = HarmoniaConstants.texts.getString("registerText");
+    public static final String registrationText = HarmoniaConstants.texts.getString("registerationText");
+    public static final String aboutMeText = HarmoniaConstants.texts.getString("aboutMeText");
+    public static final String descriptionHintText = HarmoniaConstants.texts.getString("descriptionHintText");
+    public static final String changePfpText = HarmoniaConstants.texts.getString("changePfpText");
+    public static final String saveText = HarmoniaConstants.texts.getString("saveText");
+    public static final String channleListText = HarmoniaConstants.texts.getString("channelsListText");
+    public static final String returnText = HarmoniaConstants.texts.getString("returnText");
+    public static final String serverMembersText = HarmoniaConstants.texts.getString("serverMembersText");
+    public static final String usersText = HarmoniaConstants.texts.getString("usersText");
+}
