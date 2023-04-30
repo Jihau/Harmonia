12aade4f9e3a243b2f7a5e8a8e7485268c77021a
mohammja <mohammja@metropolia.fi>
2023-04-30 18:07:00 +0300

    -Refresh the scene immediately after choosing the language. -Adding contentful for arabic and finnish -Login view localization -Adding a test for arabic localization of the login form

--- Frontend/src/main/java/com/harmonia/client/ContentfulClient.java
+++ Frontend/src/main/java/com/harmonia/client/ContentfulClient.java
@@ -6,21 +6,26 @@ import com.contentful.java.cda.CDAEntry;
 import com.contentful.java.cda.LocalizedResource;
 import com.harmonia.constants.HarmoniaMessagesConstants;
 
-import java.util.Locale;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class ContentfulClient {
 
 
     public static void loadLabels(String lang) {
+        Map<String, String[]> languageProperties = new LinkedHashMap<>();
+        languageProperties.put("en-US", new String[]{"a152txh9sqmm", "vegBUFffokA5M4QdrryQ9HRUIJm4EtEE-vymsDAVz7Y"});
+        languageProperties.put("fi", new String[]{"a152txh9sqmm", "vegBUFffokA5M4QdrryQ9HRUIJm4EtEE-vymsDAVz7Y"});
+        languageProperties.put("ar", new String[]{"b3d2vb8ir299", "gRmKfIaOIdLl2eb574v0jdClheK86Cx_oJC0ABP_dtg"});
         // Add more locales
-        if (!lang.equalsIgnoreCase("fi")) {
-            lang = "en-US";
-        } else {
+        if (lang.equalsIgnoreCase("fi") || lang.equalsIgnoreCase("ar")) {
             lang = lang.toLowerCase();
+        } else {
+            lang = "en-US";
         }
         CDAClient client = CDAClient.builder()
-                .setSpace("e1rl81rzkl0m")
-                .setToken("GzMfJ82u5ukH8cFinpMmECFGEWE-u9b6WxnqTMoMVYk")
+                .setSpace(languageProperties.get(lang)[0])
+                .setToken(languageProperties.get(lang)[1])
                 .build();
         CDAArray modelArray = client.fetch(CDAEntry.class).withLocale(lang).all();
         HarmoniaMessagesConstants.CONTENTFUL_RESOURCE = (LocalizedResource) modelArray.items().get(0);
